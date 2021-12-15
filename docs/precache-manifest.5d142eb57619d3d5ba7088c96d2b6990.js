7b358e944b9612dea53b6ee1f3eeccb7dab08d2e
alejandro mendiz <alejo12html@gmail.com>
2021-12-15 14:59:45 -0500

    docs agregado

---
+++ docs/precache-manifest.5d142eb57619d3d5ba7088c96d2b6990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f43e1c7e66f1d19f775b327e4e7e64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f5421330b73bd417840",
+    "url": "/static/css/main.de099153.chunk.css"
+  },
+  {
+    "revision": "3d35eb6e5e8d98dd0848",
+    "url": "/static/js/2.a9c059df.chunk.js"
+  },
+  {
+    "revision": "0cf5ba2c00bbc0b01089c885c528fd0c",
+    "url": "/static/js/2.a9c059df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f5421330b73bd417840",
+    "url": "/static/js/main.f52fe3f4.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
